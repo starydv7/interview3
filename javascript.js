cc20fe3ff926fc93e45a3a9c871e9516f9797aaa
starydv7 <pawanstar72926@gmail.com>
2022-11-05 23:40:09 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const add = () => {
+    return true + true + true + 1;
+}
+//true=1
+var res = add();
+console.log("add",res);
+const sub = () => {
+    return false + 2;
+}
+//false ==0
+var result = sub();
+console.log("sub",result);
