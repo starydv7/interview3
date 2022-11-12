4a0230b2a2072632613083550dc5ad0445e023bc
starydv7 <pawanstar72926@gmail.com>
2022-11-12 11:08:35 +0530

    application

---
+++ interview3/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import React, {useState ,useEffect} from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const add = () => {
+        setCount(count + 1);
+        console.log(count);
+    }
+     const minus = () => {
+       setCount(count - 1);
+       console.log(count);
+     };
+    useEffect(() => {
+        add();
+    },[]);
+    
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={add}>Add</button>
+      <button onClick={minus}>Minus</button>
+    </div>
+  );
+}
+
+export default Counter
