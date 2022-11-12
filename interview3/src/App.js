ae416a4ab80357b9e5975d8a983b4f1a764a25d2
starydv7 <pawanstar72926@gmail.com>
2022-11-12 11:29:45 +0530

    minus

--- interview3/src/App.js
+++ interview3/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import Props from './components/Props';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <Counter />
+      <Props/>
     </div>
   );
 }
---
+++ interview3/src/components/Props.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import Child from './Child';
+
+const Props = () => {
+    const [name, setName] = useState("Pawan");
+  return (
+      <div>
+          <Child name={name} />
+          <button onClick={()=>setName("Yadav")}>Change Name</button>
+    </div>
+  )
+}
+
+export default Props
