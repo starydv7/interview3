00e4a9f4f8361e0457a142e0b43ea1b98e704d8b
starydv7 <pawanstar72926@gmail.com>
2022-11-12 11:56:27 +0530

    props

---
+++ interview3/src/components/Inline.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect } from 'react'
+
+const Inline = () => {
+    useEffect(() => {
+        let age = 25;
+        let name = "Pawan";
+        if (age > 26) {
+            console.log("You are old");
+        } else if(name === "Pawan" && age ===25) {
+            console.log(`I am {age} years old`);
+            console.log("Pawan is my Name");
+            
+        }
+    
+     
+    }, [])
+    
+  return (
+    <div>Inline</div>
+  )
+}
+
+export default Inline
