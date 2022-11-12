6957356107b09d1fcf6dcfaa3e19a1b524d3d9cb
starydv7 <pawanstar72926@gmail.com>
2022-11-12 13:22:05 +0530

    input

--- interview3/src/components/EventHandler.jsx
+++ interview3/src/components/EventHandler.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 
 const EventHandler = () => {
-    const [num12, setNum1] = useState("");
+    const [num12, setNum1] = useState(0);
     const getInput = (e) => {
         console.log(e.target.value);
+
     }
     const addNum = (num1, num2) => {
         console.log(num1 + num2);
@@ -13,8 +14,8 @@ const EventHandler = () => {
       <div>
           <h3>{num12}</h3>
           <input placeholder='add something..'
-              onChange={getInput} value="value" />
-          <button onClick={()=>addNum(2,3)}>Add Number </button>
+              onChange={getInput} />
+          <button onClick={()=>addNum(414,2541)}>Add Number </button>
     </div>
   )
 }
