2200b17a0afe284c5b5b02416171030ab5c62b1a
starydv7 <pawanstar72926@gmail.com>
2022-11-13 22:30:16 +0530

    useEffect

--- authentication/src/components/Home.jsx
+++ authentication/src/components/Home.jsx
@@ -1,7 +1,20 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const getData =()=>{
+      axios
+        .get("https://apipawan3.herokuapp.com/users")
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        });
+  }
+  useEffect(() => {
+    getData();
+  }, [data])
+  
   return (
     <div>Home</div>
   )
