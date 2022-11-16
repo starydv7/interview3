589155412cbc246ac0b1018995984162b311e832
starydv7 <pawanstar72926@gmail.com>
2022-11-16 23:20:13 +0530

    msg

---
+++ Ad.js
@@ -0,0 +1,13 @@
+  function findComplement(num) {
+           let ans = 0;
+           for (let i = 0; num > 0; i++) {
+               ans += Math.pow(2, i) * (!(num % 2));
+               num = Math.floor(num / 2);
+           }
+           return ans;
+       }
+ 
+       // Driver code
+ 
+       let N = 50;
+       console.log(findComplement(N));
