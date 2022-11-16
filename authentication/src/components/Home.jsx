11eb687c3ca79d343e8c84258707c2eef0b7fc07
starydv7 <pawanstar72926@gmail.com>
2022-11-16 10:27:45 +0530

    fetch and axios

--- authentication/src/components/Home.jsx
+++ authentication/src/components/Home.jsx
@@ -3,20 +3,34 @@ import React, { useState, useEffect } from 'react';
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const getData =()=>{
-      axios
-        .get("https://apipawan3.herokuapp.com/users")
-        .then((res) => {
-          console.log(res.data);
-          setData(res.data);
-        });
+  // const getData =()=>{
+  //     axios
+  //       .get("https://apipawan3.herokuapp.com/users")
+  //       .then((res) => {
+  //         console.log(res.data);
+  //         setData(res.data);
+  //       });
+  // }
+  const getData = () => {
+    fetch("https://apipawan3.herokuapp.com/users")
+    .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+    })
   }
   useEffect(() => {
     getData();
   }, [])
   
   return (
-    <div>Home</div>
+    <div>
+      {data.map((item)=> {
+        <div key={item.id}>
+          <p>{item.name}</p>
+        </div>;
+         
+      })}
+    </div>
   )
 }
 
